202101ac54842088749f786821d9ca74105f0369
JaydenMoore <jaydenmoore456@gmail.com>
2023-10-25 21:20:17 -0400

    Jayden Moore added decode function

--- encode_and_decode.py
+++ encode_and_decode.py
@@ -21,6 +21,16 @@ def encode(number):  # Takes a number, and adds 3 to each digit in the number!
 
 # TODO: Add the Decode function!
 
+#Decode function implemented by Jayden Moore
+#Takes an encoded password and decodes it with the inverse of the encode method
+def decode(password):
+    decoded_password = ""
+
+    #Subtracts 3 from each integer within the password's string
+    for i in password:
+        decoded_password += str(int(i) - 3)
+    return decoded_password
+
 if __name__ == "__main__":
     
     """
